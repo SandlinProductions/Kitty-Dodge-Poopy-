847ebb0e42ad5ae310b29a05f16d89cef902b291
Stephen Sandlin <info@SandlinProds.com>
2021-01-16 20:59:39 -0500

    Double Points added

--- Kitty Dodge Poopy!/Assets/Scripts/PowerUpCounter.cs
+++ Kitty Dodge Poopy!/Assets/Scripts/PowerUpCounter.cs
@@ -8,6 +8,8 @@ public class PowerUpCounter : MonoBehaviour {
     public int score;
     public static float highscore;
     public PointCounter pointCounter;
+    public GameObject doublePointText;
+    private bool doublePoints;
     // Use this for initialization
     void Start()
     {
@@ -31,21 +33,67 @@ public class PowerUpCounter : MonoBehaviour {
         //checks collider's tag
         if (collision.gameObject.tag == "PowerUp")
         {
-            score += 1;
-            //increments score by 1
-            Debug.Log("1 Points");
+            if (doublePoints == true)
+            {
+                score += 2;
+                //increments score by 2
+                Debug.Log("2 Points (Double Time!)");
+            }
+            else
+            {
+                score += 1;
+                //increments score by 1
+                Debug.Log("1 Points");
+            }
         }
         else if (collision.gameObject.tag == "PowerBag")
         {
-            score += 2;
-            //increments score by 2
-            Debug.Log("2 Points!");
+            if (doublePoints == true)
+            {
+                score += 4;
+                //increments score by 4
+                Debug.Log("4 Points (Double Time!)");
+            }
+            else
+            {
+                score += 2;
+                //increments score by 2
+                Debug.Log("2 Points!");
+            }
         }
         else if (collision.gameObject.tag == "PizzaPower")
         {
-            score += 3;
-            //increments score by 3
-            Debug.Log("3 Points!");
+            if (doublePoints == true)
+            {
+                score += 6;
+                //increments score by 2
+                Debug.Log("6 Points (Double Time!)");
+            }
+            else
+            {
+                score += 3;
+                //increments score by 3
+                Debug.Log("3 Points!");
+            }
         }
+        else if (collision.gameObject.tag == "TimesTwo")
+        {
+            StartCoroutine(Timestwotime());
+        }
+    }
+    IEnumerator Timestwotime()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Double Points at timestamp : " + Time.time);
+        doublePoints = true;
+        doublePointText.SetActive(true);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(10);
+        doublePoints = false;
+        doublePointText.SetActive(false);
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Double Points at timestamp : " + Time.time);
+
     }
 }
